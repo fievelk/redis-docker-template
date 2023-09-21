91ba6d540d2a407e3d81ad7a4f1cf60bcf3b6f78
Pierpaolo Pantone <24alsecondo@gmail.com>
2023-09-21 19:20:19 +0200

    Use StrictRedis and decode responses

--- redis_experiments/receiver_service/app.py
+++ redis_experiments/receiver_service/app.py
@@ -4,7 +4,7 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host="redis", port=6379)
+cache = redis.StrictRedis(host="redis", port=6379, decode_responses=True)
 
 
 def _receive_data():
